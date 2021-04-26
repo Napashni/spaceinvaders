ac70922435b4b4f0e7a8fa5fecd1598ac8bd1ae2
Quentin <quentin.foulon2002@gmail.com>
2021-04-26 10:39:13 +0200

    ajout de la limitation de l'espace de jeux

---
+++ src/main/java/fr/unilim/iut/SpaceInvaders/HorsEspaceJeuException.java
@@ -0,0 +1,10 @@
+package fr.unilim.iut.SpaceInvaders;
+
+@SuppressWarnings("serial")
+class HorsEspaceJeuException extends RuntimeException {
+
+    public HorsEspaceJeuException(String message) {
+	    super(message);
+    }
+
+}
