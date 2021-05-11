972d25d39c57438bcc36a0808e8e5097d18cf82c
Quentin <quentin.foulon2002@gmail.com>
2021-05-11 11:29:19 +0200

    Déplacer le vaisseau vers la gauche dans l'espace de jeu

--- src/main/java/fr/unilim/iut/SpaceInvaders/SpaceInvaders.java
+++ src/main/java/fr/unilim/iut/SpaceInvaders/SpaceInvaders.java
@@ -65,4 +65,8 @@ public class SpaceInvaders {
 		public void deplacerVaisseauVersLaDroite() {
 	        if (vaisseau.abscisse()< (longueur-1)) vaisseau.seDeplacerVersLaDroite();
 		}
+
+		public void deplacerVaisseauVersLaGauche() {
+			if (vaisseau.abscisse() > 1) vaisseau.seDeplacerVersLaGauche();			
+		}
    }
